33d861d375ed1c35a7065265b6b00409ed43950c
Harshit Maheshwari <hmharshit@gmail.com>
2018-09-14 13:50:07 +0530

    image size fixed

--- register.php
+++ register.php
@@ -1,3 +1,15 @@
 <?php
-echo "hello"
+require_once('connect.php');
+echo '<pre>'; print_r($_POST); echo '</pre>';
+// echo implode(",", $_POST['nomination']['category_list']);
+if(isset($_POST) & !empty($_POST)) {
+
+}
+
+$yrls = array();
+
+for ($i=0; $i < sizeof($_POST['nomination']['category_list']) ; $i++) { 
+	// $yrls[]=$_POST['nomination']['category_list'][$i]['url']
+}
+echo implode(",", $yrls);
 ?>
