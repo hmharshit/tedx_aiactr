3a3126375d0559f3758143a04bf2776d163781db
Harshit Maheshwari <hmharshit@gmail.com>
2018-09-14 09:30:06 +0530

    testing

--- connect.php
+++ connect.php
@@ -1,8 +1,16 @@
 <?php
-
-echo "hello";
-
-$hello = getenv("RDS_HOST");
-echo $hello;
-echo $_ENV;
+phpinfo();
+echo extension_loaded('pgsql') ? 'yes':'no';
+$localhost = "tedx-aiactr.cbls8rqpglrt.ap-south-1.rds.amazonaws.com";
+$DBNAME = "tedx_aiactr";
+$USERNAME = "tedx_aiactr";
+$PASSWORD = "3sy43hxvcUDW4KQV";
+$connection_string = "host=$localhost dbname=$DBNAME user=$USERNAME password=$PASSWORD";
+echo $connection_string;
+$db_connection = pg_connect($connection_string);
+if($db_connection) {
+       echo 'connected';
+    } else {
+        echo 'there has been an error connecting';
+    } 
 ?>
