1945b1ba78e397d9f3c3debba9c0ab41c720eab6
Hemant Rawat <hemantrawat9582@gmail.com>
2018-10-06 17:02:38 +0530

    link color

--- header.php
+++ header.php
@@ -1,31 +1,31 @@
 <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark gallNav">
-    <a class="navbar-brand" href="index.html#themeDiv"><img width="150" src="public/images/TEDxAIACT.png" alt="" title="TEDxAIACTR"></a>
+    <a class="navbar-brand" href="index.php"><img width="150" src="public/images/TEDxAIACT.png" alt="" title="TEDxAIACTR"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapseNav" aria-controls="collapseNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="collapseNav">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link scrollable" href="index.php">Home</a>
+            <li class="nav-item">
+                <a class="nav-link scrollable" href="index.php"><font color="white">Home</font> </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link scrollable" href="about.php">About</a>
+                <a class="nav-link scrollable" href="about.php"><font color="white">About</font></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link scrollable" href="speaker.php">Speakers</a>
+                <a class="nav-link scrollable" href="speaker.php"><font color="white">Speakers</font></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link scrollable" href="sponser.php">Sponsors</a>
+                <a class="nav-link scrollable" href="sponser.php"><font color="white">Sponsors</font></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link scrollable" href="team.php">Team</a>
+                <a class="nav-link scrollable" href="team.php"><font color="white">Team</font></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link scrollable" href="theme.php">Theme</a>
+                <a class="nav-link scrollable" href="theme.php"><font color="white">Theme</font></a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link scrollable" href="registration.php">Registration</a>
+            <li class="nav-item">
+                <a class="nav-link scrollable" href="registration.php"><font color="white">Registration</font></a>
             </li>
 
         </ul>
