900075398ac30e6122d8f24d8a60a5f658648b2d
Hemant Rawat <hemantrawat9582@gmail.com>
2018-10-10 22:02:46 +0530

    about speaker footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
 
 <div class="row" id="footer" style="background-image: url('public/images/college1.jpeg'); background-size: cover; opacity: 0.6">
     <div class="col-sm-12 col-md-6">
-        <h3><font color="#999999">Important Links</h3>
-        <a target="_blank" href="https://www.ted.com/"><font color="#cccccc"><strong>TED</strong></font></a><br>
-        <a target="_blank" href="https://www.ted.com/about/programs-initiatives/tedx-program"><font color="#cccccc"><strong>TEDx</strong></font></a><br>
-        <a target="_blank" href="https://blog.ted.com/"><font color="#cccccc"><strong>TED Blog</strong></font></a>
+        <h3><font color="#ffffff">Important Links</h3>
+        <a target="_blank" href="https://www.ted.com/"><font color="#ffffff"><strong>TED</strong></font></a><br>
+        <a target="_blank" href="https://www.ted.com/about/programs-initiatives/tedx-program"><font color="#ffffff"><strong>TEDx</strong></font></a><br>
+        <a target="_blank" href="https://blog.ted.com/"><font color="#ffffff"><strong>TED Blog</strong></font></a>
     </div>
     <div class="col-sm-12 col-md-6">
-        <h3><font color="#999999">Curator</font></h3>
-        <span><font color="#cccccc"><strong>Shashank Narayan</strong></font></span></br>
-        <span><font color="#cccccc"><strong>+91-9555691165</strong></font></span></br>
+        <h3><font color="#ffffff">Curator</font></h3>
+        <span><font color="#ffffff"><strong>Shashank Narayan</strong></font></span></br>
+        <span><font color="#ffffff"><strong>+91-9555691165</strong></font></span></br>
         <a target="_blank" href="mailto:info@tedxaiactr.com"><i class="fa fa-envelope"></i></a>
         <a target="_blank" href="https://www.facebook.com/narayanshashank"><i class="fa fa-facebook"></i></a>
         <a target="_blank" href="https://www.linkedin.com/in/narayanshashank"><i class="fa fa-linkedin"></i></a>
@@ -18,7 +18,7 @@
     <hr>
     <div class="row">
         <div class="col-sm-12">
-            <h6>&nbsp;&nbsp;&nbsp;&copy; This independent TEDx event is operated under license from TED.<</style></h6>
+            <h6>&nbsp;&nbsp;&nbsp;<font color="#ffffff">&copy; This independent TEDx event is operated under license from TED.</font></style></h6>
         </div>
     </div>
 </div>
