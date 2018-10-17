505dd848ba536114ae981ed3657433195467c30b
prabhat <prabhat.04011998@gmail.com>
2018-10-17 19:42:48 +0530

    check

--- connect.php
+++ connect.php
@@ -5,9 +5,9 @@ $DBNAME = getenv("DATABASE_NAME");
 $USERNAME = getenv("RDS_USER");
 $PASSWORD = getenv("RDS_PASSWORD");
 $connection_string = "host=$localhost dbname=$DBNAME user=$USERNAME password=$PASSWORD";
-$db_connection = pg_connect($connection_string);
+$db_connection =pg_connect($connection_string);
 if($db_connection) {
     } else {
         echo 'there has been an error connecting';
-    } 
+    }
 ?>
